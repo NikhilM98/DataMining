47dd6aecd64a3380aa1adc81e7beded9496ac728
NikhilM98 <nikhilmehra998@gmail.com>
2019-01-06 01:23:13 +0530

    Hello World

---
+++ hello_world.R
@@ -0,0 +1,17 @@
+# Learning R
+1 + 6
+x <- "Nikhil"
+x
+age = 20
+birthday <- as.Date("1998-10-23")
+assign("sport", "Futtball")
+class(x)
+class(age)
+class(sport)
+class(birthday)
+rm(sport)
+class(sport)
+is.numeric(age)
+is.numeric(x)
+is.numeric(birthday)
+nchar(x)
