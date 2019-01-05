1848fee9c71c828428167d11a13b02c6a339aafd
NikhilM98 <nikhilmehra998@gmail.com>
2019-01-06 05:11:15 +0530

    Intro to vectors

---
+++ intro_vectors.R
@@ -0,0 +1,17 @@
+# Vectors
+vec <- c(1, 3, 9)
+vec2 <- c("Alpha", "Beta", "Charlie")
+vec ^ 2
+sqrt(vec)
+seq1 <- 1:10
+seq1
+seq2 <- -1:-10
+seq2*-1
+seq1 - seq2
+seq1 * seq2
+length(2:14)
+seq1 < 4
+any(seq1 < 4)
+all(seq1 < 4)
+vec2[1]
+vec2[1:2]
